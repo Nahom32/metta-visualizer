2545a74c1341ccf0e73815d5575b5776efad8364
Nahom32 <senaynahom00@gmail.com>
2024-03-09 09:12:41 +0300

    added a reader

---
+++ infrastructure/metta_reader.py
@@ -0,0 +1,11 @@
+from typing import List
+def read_metta_file(url_path: str) -> List[str]:
+    raw_data = []
+    with open(url_path, 'r') as file:
+        for  line in file:
+            raw_data.append(line)
+    
+            
+            
+        
+    
