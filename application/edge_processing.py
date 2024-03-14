c6bbae81d232dc4b462150e09a1c6afa3b08c208
Nahom32 <senaynahom00@gmail.com>
2024-03-14 10:18:51 +0300

    added to search with a single id

--- application/edge_processing.py
+++ application/edge_processing.py
@@ -5,13 +5,12 @@ from infrastructure.metta_reader import read_metta_file
 def process_edges(edge_path: str, nodes: List[Node],descriptors: List[Edge],id: str) -> List[Edge]:
     edges_raw = read_metta_file(edge_path)
     print(nodes)
-    edges_raw.pop()
     edge = {}
     list_of_edges = []
     count = 1
     for i in edges_raw:
         temp_edge = i.strip()[1:-1].split()
-        if count%2 == 0:
+        if len(temp_edge) == 7:
             for j in nodes:
                 if j.props[id] == temp_edge[3][:-1]:
                     for k in descriptors:
@@ -25,6 +24,55 @@ def process_edges(edge_path: str, nodes: List[Node],descriptors: List[Edge],id: 
                             edge = {}
         count+=1                
     return list_of_edges
+
+def process_edges_one(edge_path: str,process_str: str,nodes: List[Node],descriptors: List[Edge],id: str):
+    edges_raw = read_metta_file(edge_path)
+    print(nodes)
+    edges_raw.pop()
+    edge = {}
+    node = Node()
+    edges_list = []
+    # for i in edges_raw:
+    #     temp_edge = i.strip()[1:-1].split()
+    #     for j in nodes:
+    #         if process_str == j.props[id]:
+    #             node.props = j.props
+    #             break
+    #     if node.props == {}:
+    #         raise Exception("The node doesn't exist")
+    #     for k in temp_edge:
+    for j in nodes:
+        if j.props[id] == process_str:
+            print("hi", j)
+            node.props = j.props
+    print("This the node props", node.props)
+    if node.props == {}:
+        raise Exception("The node doesn't exist")
+    for j in edges_raw:
+        temp_edge = j.strip()[1:-1].split()
+        print(temp_edge)
+        print("This is a temp_edge", temp_edge[3][:-1])
+        print("this is node props id", node.props[id])
+        if temp_edge[3][:-1] == node.props[id]:
+            print("hello how")
+            for i in descriptors:
+                if str(temp_edge[5][:-2]) == str(i.props[id]):
+                    edge_prop = {}
+                    edge_prop['node'] = node
+                    edge_prop['descriptor'] = i
+                    edge_prop['value'] = temp_edge[-1][:-1]
+                    edges_list.append(Edge(**edge_prop))
+    return edges_list
+            
+            
+            
+                
+                
+        
+        
+        
+    
+    
 
                 
                             
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 
 from application.node_processing import change_to_node_from_mult_files
-from application.edge_processing import process_edges
+from application.edge_processing import process_edges_one,process_edges
 from application.graph_processing import get_graph
 from presentation.draw_graph import draw_graph
 import igraph as ig
@@ -8,7 +8,8 @@ import igraph as ig
 nodes = change_to_node_from_mult_files("test_files/compound_nodes.metta")
 descriptors = change_to_node_from_mult_files("test_files/descriptor_nodes.metta")
 
-edges = process_edges("test_files/compound_edges.metta",nodes, descriptors,"id")
+edges = process_edges_one("test_files/compound_edges.metta","CID2499094",nodes, descriptors,"id")
+print("This are the edges: ", edges)
 graph = get_graph(edges)
 draw_graph(graph)
 
